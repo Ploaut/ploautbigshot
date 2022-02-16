41859b266c8bcb88a7e1f98d43bfc6431135432f
Ploaut <ploautkr@gmail.com>
2022-02-17 06:03:34 +0900

    second

--- scanf.c
+++ scanf.c
@@ -8,12 +8,13 @@ int main()
 	int height = 0;
 
 	
-	printf("당신의 나이를 입력하세요 :");
+	printf("당신의 나이를 입력하세요! :");
 	scanf_s("%d", &age);
-	printf("당신의 몸무게를 입력하세요 : ");
+	printf("당신의 몸무게를 입력하세요! : ");
 	scanf_s("%d", &weight);
-	printf("당신의 키를 입력하세요 :");
+	printf("당신의 키를 입력하세요! :");
 	scanf_s("%d", &height);
 
 	printf("__________________________________\n\n");
 	printf("당신의 나이는 %d 살 몸무게는 %d kg 키는 %d cm 이군요!", age, weight, height);
+}
