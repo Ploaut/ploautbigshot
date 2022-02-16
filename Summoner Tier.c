1b2eef0e1f15bba0c7cd694027be98575aeba58b
Ploaut <ploautkr@gmail.com>
2022-02-17 06:00:00 +0900

    fail.c

---
+++ Summoner Tier.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char B[] = "Bronze";
+	char S[] = "Silver";
+	char G[] = "Gold";
+	char P[] = "Platinum";
+	char D[] = "Diamond";
+	char M[] = "Master";
+	char Gm[] = "GrandMaster";
+	char C[] = "Challenger";
+
+
+	printf("1번 소환사님의 티어는 %s 입니다.\n", B);
+	printf("2번 소환사님의 티어는 %s 입니다.\n", S);
+	printf("3번 소환사님의 티어는 %s 입니다.\n", G);
+	printf("4번 소환사님의 티어는 %s 입니다.\n,", P);
+	printf("5번 소환사님의 티어는 %s 입니다.\n", D);
+
+	
+
+
+	return 0;
+}
---
+++ fail.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main(void)
+
+{
+	int age;
+	double weight, height;
+
+	
+	printf("나이를 입력하세요:");
+	scanf_s("%d", &age);
+	printf("키를 입력하세요:");
+	scanf_s("%.1lf", &height);
+	printf("몸무게를 입력하세요:");
+	scanf_s("%.1lf",&weight);
+	
+	printf("---------------------------\n");
+	printf("당신의 나이는 %d살 키는%.1lf cm,몸무게는 %.1lf kg 이군요!", age, height, weight);
+
+	
+	return 0;
+}
+
+//실패본
